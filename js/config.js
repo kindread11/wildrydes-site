0a8310aa18b7cf844161a5e7b0f368d9f1ca06f1
root <root@ip-172-31-34-19.ap-northeast-2.compute.internal>
2025-02-27 07:08:16 +0000

    V6

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-2_VGTAWAOzl', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'fvru5cee1rfrmev8t4n4ll9ts', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-2_Hm3ot20ir', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'fr6q1mhjchnhdijv03dtttutk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
